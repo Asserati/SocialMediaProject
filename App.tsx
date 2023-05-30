768b3fcbd2eee742dc7ef8c2958fc3e2c70ae99a
unknown <igniux527@gmail.com>
2023-05-31 00:22:24 +0300

    init project and install vector icons

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import colors from './src/theme/colors';
+import fonts from './src/theme/fonts';
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
---
+++ src/theme/fonts.ts
@@ -0,0 +1,19 @@
+const size = {
+    xs: 10,
+    small: 12,
+    default: 14,
+    medium: 16,
+    large: 18,
+    xl: 20,
+    xxl: 22,
+}
+
+const weight = {
+    full: '900',
+    semi: '600',
+    bold: 'bold',
+    normal: 'normal',
+    thin: '400',
+}
+
+export {weight,size};
